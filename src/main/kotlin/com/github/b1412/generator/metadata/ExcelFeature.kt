11abea967b61165b8ba3ee1863ea2fb477b01538
Leon Zhou <zhouleib1412@gmail.com>
2020-12-08 15:00:22 +1300

    add index to excelfeature

--- src/main/kotlin/com/github/b1412/generator/metadata/ExcelFeature.kt
+++ src/main/kotlin/com/github/b1412/generator/metadata/ExcelFeature.kt
@@ -3,8 +3,9 @@ package com.github.b1412.generator.metadata
 @Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ExcelFeature(
-        val importable: Boolean = false,
-        val exportable: Boolean = false,
-        val column: String = "",
-        val header: String = ""
+    val importable: Boolean = false,
+    val exportable: Boolean = false,
+    val column: String = "",
+    val header: String = "",
+    val index: Int
 )
