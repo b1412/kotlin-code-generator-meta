4b5cc9c0b95736f439f3e398dd87d18e8754a5ac
Leon Zhou <zhouleib1412@gmail.com>
2020-12-29 15:29:05 +1300

    add permission ignore

---
+++ src/main/kotlin/com/github/b1412/generator/metadata/PermissionFeatureIgnore.kt
@@ -0,0 +1,5 @@
+package com.github.b1412.generator.metadata
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class PermissionFeatureIgnore
